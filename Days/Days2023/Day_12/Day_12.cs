9b7c5789c2343bbeeb07d8c22f736cc5153cfb24
Robin Laevaert <robin.laevaert@protonmail.com>
2023-12-13 14:45:00 +0100

    2023/12+13

--- Days/Days2023/Day_12/Day_12.cs
+++ Days/Days2023/Day_12/Day_12.cs
@@ -1,25 +1,155 @@
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+
 namespace Days2023;
+
 
 public class Day_12 : Day
 {
-public Day_12()
-{
-	Title = "";
-	DayNumber = 12;
-	Year = 2023;
-}
-public override void Gather_input()
-{
+	private List<Tuple<string, int[]>> lines;
 
+	public Day_12()
+	{
+		Title = "Hot Springs";
+		DayNumber = 12;
+		Year = 2023;
+	}
+
+	public override void Gather_input()
+	{
+		lines = Input.Where(x => !string.IsNullOrWhiteSpace(x)).Select(x =>
+				Tuple.Create(
+					x.Split(' ').First(),
+					x.Split(' ').Last()
+						.Split(',')
+						.Select(int.Parse)
+						.ToArray()))
+			.ToList();
+	}
+
+	public override string HandlePart1()
+	{
+		var CorrectCounters = new ConcurrentBag<int>();
+
+		Parallel.ForEach(lines, line =>
+		{
+			var combinations = Combinations(line.Item1);
+			var combinationsEvalued =
+				combinations.Select(x => x.Split('.').Select(c => c.Length).Where(c => c != 0).ToArray())
+					.Where(x => x.SequenceEqual(line.Item2));
+			CorrectCounters.Add(combinationsEvalued.Count());
+		});
+
+		return CorrectCounters.Sum().ToString();
+	}
+
+	public static IEnumerable<string> Combinations(string input)
+	{
+		var replacementChars = "#.";
+
+		var sets = input.Select(ch => ch == '?' ? replacementChars : ch.ToString());
+
+		return Combine(sets).Select(x => new string(x.ToArray()));
+	}
+
+	public static IEnumerable<IEnumerable<T>> Combine<T>(IEnumerable<IEnumerable<T>> sequences)
+	{
+		IEnumerable<IEnumerable<T>> emptyProduct = new[] { Enumerable.Empty<T>() };
+
+		return sequences.Aggregate(
+			emptyProduct,
+			(accumulator, sequence) =>
+				from accseq in accumulator
+				from item in sequence
+				select accseq.Concat(new[] { item }));
+	}
+
+	public override string HandlePart2()
+	{
+		var CorrectCounters = new ConcurrentBag<long>();
+
+		Parallel.ForEach(lines, line =>
+		{
+			CorrectCounters.Add(CalculateRow(string.Join('?', Enumerable.Range(0, 5).Select(x => line.Item1)),
+				ImmutableStack.CreateRange(Enumerable.Range(0, 5).Select(x => line.Item2).SelectMany(x => x).Reverse()),
+				new Dictionary<State, long>()));
+		});
+
+		return CorrectCounters.Sum().ToString();
+	}
+
+
+	private long CalculateRow(string row, ImmutableStack<int> nums,
+		Dictionary<State, long> cache)
+	{
+		if (!cache.ContainsKey(new (row, nums)))
+		{
+			cache[new(row, nums)] = row.FirstOrDefault() switch
+			{
+				// Operational => remove dot and compute again
+				'.' => CalculateRow(row[1..], nums, cache),
+				// Handle as if it's both . and # if ?
+				'?' => CalculateRow("." + row[1..], nums, cache) + CalculateRow("#" + row[1..], nums, cache),
+				'#' => ActuallyDoSomething(row, nums, cache),
+				// If any numbers left => Not correct; return 0 else correct and return 1
+				_ => nums.Any() ? 0 : 1,
+			};
+		}
+
+		return cache[new(row, nums)];
+	}
+
+	private long ActuallyDoSomething(string row, ImmutableStack<int> nums,
+		Dictionary<State, long> cache)
+	{
+		// No number left even tho we have a damaged spring => not correct
+		if (!nums.Any()) return 0;
+
+		var nextNumber = nums.Peek();
+		nums = nums.Pop();
+
+		var maximumPossibleDeadSprings = row.TakeWhile(s => s is '#' or '?').Count();
+
+		// number = 5, first part of row = #??# => false
+		if (maximumPossibleDeadSprings < nextNumber) return 0;
+
+		// End of the line
+		if (row.Length == nextNumber) return CalculateRow("", nums, cache);
+
+		// number = 5, first part of row = ##???#??# => false, number can never be 5 (either 3,4 or 6+)
+		if (row[nextNumber] == '#') return 0;
+
+		return CalculateRow(row[(nextNumber + 1)..], nums, cache);
+	}
 }
 
-public override string HandlePart1()
+public class State : IEquatable<State>
 {
-	return "";
-}
+	public State(string row, ImmutableStack<int> validator)
+	{
+		Row = row;
+		Validator = validator;
+	}
+	public string Row { get; set; }
+	public ImmutableStack<int> Validator { get; set; }
 
-public override string HandlePart2()
-{
-	return "";
-}
+	public bool Equals(State? other)
+	{
+		if (ReferenceEquals(null, other)) return false;
+		if (ReferenceEquals(this, other)) return true;
+		return Row == other.Row && Validator.Equals(other.Validator);
+	}
+
+	public override bool Equals(object? obj)
+	{
+		if (ReferenceEquals(null, obj)) return false;
+		if (ReferenceEquals(this, obj)) return true;
+		if (obj.GetType() != this.GetType()) return false;
+		return Equals((State)obj);
+	}
+
+	public override int GetHashCode()
+	{
+		return HashCode.Combine(Row, Validator);
+	}
 }
--- Days/Days2023/Day_13/Day_13.cs
+++ Days/Days2023/Day_13/Day_13.cs
@@ -2,24 +2,219 @@ namespace Days2023;
 
 public class Day_13 : Day
 {
-public Day_13()
-{
-	Title = "";
-	DayNumber = 13;
-	Year = 2023;
-}
-public override void Gather_input()
-{
+	private List<Pattern> Patterns;
+	public Day_13()
+	{
+		Title = "Point of Incidence";
+		DayNumber = 13;
+		Year = 2023;
+	}
 
+	public override void Gather_input()
+	{
+		Patterns = new List<Pattern>();
+		var temp = new Pattern();
+		foreach (var line in Input)
+		{
+			if (string.IsNullOrWhiteSpace(line))
+			{
+				Patterns.Add(temp);
+				temp = new Pattern();
+				continue;
+			}
+
+			temp.Lines.Add(line);
+		}
+
+		if (temp.Lines.Any()) Patterns.Add(temp);
+	}
+
+	public override string HandlePart1()
+	{
+		var points = 0l;
+		var patternIndex = 0;
+		foreach (var pattern in Patterns)
+		{
+			var possibleLineSplits = Enumerable.Range(0, pattern.Lines.First().Length).ToList();
+			foreach (var line in pattern.Lines)
+			{
+				if (!possibleLineSplits.Any()) break;
+
+				possibleLineSplits = possibleLineSplits.Where((x,i) =>
+				{
+					var left = new string(line[..possibleLineSplits[i]].Reverse().ToArray());
+					var right = line[possibleLineSplits[i]..];
+
+					var isMirrorPossible = left.Any() && right.Any();
+					var smallestLength = Math.Min(left.Length, right.Length);
+					isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
+					return isMirrorPossible;
+				}).ToList();
+			}
+
+			if (possibleLineSplits.Count == 1)
+			{
+				points += possibleLineSplits.First();
+				continue;
+			}
+			
+			var possibleColumnSplits = Enumerable.Range(0, pattern.Lines.Count).ToList();
+			for (int i = 0; i < pattern.Lines.First().Length; i++)
+			{
+				if (!possibleColumnSplits.Any()) break;
+				
+				var str = pattern.Lines.Aggregate("", (current, line) => current + line[i]);
+				
+				possibleColumnSplits = possibleColumnSplits.Where((x,ind) =>
+				{
+					var left = new string(str[..possibleColumnSplits[ind]].Reverse().ToArray());
+					var right = str[possibleColumnSplits[ind]..];
+
+					var isMirrorPossible = left.Any() && right.Any();
+					var smallestLength = Math.Min(left.Length, right.Length);
+					isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
+					return isMirrorPossible;
+				}).ToList();
+			}
+			if (possibleColumnSplits.Count == 1)
+			{
+				points += possibleColumnSplits.First() * 100;
+				continue;
+			}
+		}
+
+		return points.ToString();
+	}
+
+	public override string HandlePart2()
+	{
+		var points = 0l;
+		var patternIndex = 0;
+		foreach (var pattern in Patterns)
+		{
+			var ogPatternPoints = 0;
+			var possibleLineSplits1 = Enumerable.Range(0, pattern.Lines.First().Length).ToList();
+			foreach (var line in pattern.Lines)
+			{
+				if (!possibleLineSplits1.Any()) break;
+				possibleLineSplits1 = possibleLineSplits1.Where((x,i) =>
+				{
+					var left = new string(line[..possibleLineSplits1[i]].Reverse().ToArray());
+					var right = line[possibleLineSplits1[i]..];
+
+					var isMirrorPossible = left.Any() && right.Any();
+					var smallestLength = Math.Min(left.Length, right.Length);
+					isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
+					return isMirrorPossible;
+				}).ToList();
+			}
+
+			if (possibleLineSplits1.Count == 1)
+			{
+				ogPatternPoints += possibleLineSplits1.First();
+			}
+
+			if (ogPatternPoints == 0)
+			{
+				var possibleColumnSplits1 = Enumerable.Range(0, pattern.Lines.Count).ToList();
+				for (var i = 0; i < pattern.Lines.First().Length; i++)
+				{
+					if (!possibleColumnSplits1.Any()) continue;
+				
+					var str = pattern.Lines.Aggregate("", (current, line) => current + line[i]);
+				
+					possibleColumnSplits1 = possibleColumnSplits1.Where((x,ind) =>
+					{
+						var left = new string(str[..possibleColumnSplits1[ind]].Reverse().ToArray());
+						var right = str[possibleColumnSplits1[ind]..];
+
+						var isMirrorPossible = left.Any() && right.Any();
+						var smallestLength = Math.Min(left.Length, right.Length);
+						isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
+						return isMirrorPossible;
+					}).ToList();
+				}
+				if (possibleColumnSplits1.Count == 1)
+				{
+					ogPatternPoints += possibleColumnSplits1.First() * 100;
+				}
+			}
+			
+			
+			var currPatternPoints = 0;
+			for (var p = 0; p < (pattern.Lines.Count * pattern.Lines.First().Length); p++)
+			{
+				var curPattern = pattern.Lines.ToList();
+				if (currPatternPoints != 0) continue;
+				var lineNumber = Convert.ToInt32(Math.Floor((decimal)p/curPattern.First().Length));
+				var charNumber = (p % curPattern.First().Length);
+				var charToSwitch = curPattern[lineNumber][charNumber];
+				curPattern[lineNumber] = curPattern[lineNumber].Remove(charNumber, 1)
+					.Insert(charNumber, charToSwitch == '#' ? "." : "#");
+				
+				var possibleLineSplits = Enumerable.Range(0, curPattern.First().Length).ToList();
+
+
+				foreach (var line in curPattern)
+				{
+					if (!possibleLineSplits.Any()) break;
+
+					possibleLineSplits = possibleLineSplits.Where((x,i) =>
+					{
+						var left = new string(line[..possibleLineSplits[i]].Reverse().ToArray());
+						var right = line[possibleLineSplits[i]..];
+
+						var isMirrorPossible = left.Any() && right.Any();
+						var smallestLength = Math.Min(left.Length, right.Length);
+						isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
+						return isMirrorPossible;
+					}).ToList();
+				}
+				
+				if (possibleLineSplits.Count(x => x != ogPatternPoints) == 1)
+				{
+					currPatternPoints = possibleLineSplits.First(x => x != ogPatternPoints);
+					continue;
+				}
+			
+				var possibleColumnSplits = Enumerable.Range(0, curPattern.Count).ToList();
+				for (int i = 0; i < curPattern.First().Length; i++)
+				{
+					if (!possibleColumnSplits.Any()) break;
+				
+					var str = curPattern.Aggregate("", (current, line) => current + line[i]);
+				
+					possibleColumnSplits = possibleColumnSplits.Where((x,ind) =>
+					{
+						var left = new string(str[..possibleColumnSplits[ind]].Reverse().ToArray());
+						var right = str[possibleColumnSplits[ind]..];
+
+						var isMirrorPossible = left.Any() && right.Any();
+						var smallestLength = Math.Min(left.Length, right.Length);
+						isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
+						return isMirrorPossible;
+					}).ToList();
+				}
+
+				if (possibleColumnSplits.Count(x => x*100 != ogPatternPoints) == 1)
+				{
+					currPatternPoints += possibleColumnSplits.First(x => x*100 != ogPatternPoints) * 100;
+					continue;
+				}
+			}
+
+			if (currPatternPoints != 0)
+			{
+				points += currPatternPoints;
+			}
+			patternIndex++;
+		}
+
+		return points.ToString();
+	}
 }
 
-public override string HandlePart1()
+public class Pattern
 {
-	return "";
-}
-
-public override string HandlePart2()
-{
-	return "";
-}
+	public List<string> Lines { get; set; } = new List<string>();
 }
