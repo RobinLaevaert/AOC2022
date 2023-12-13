773122e969af2802029f4a33d4815f134e5fa4f1
Robin Laevaert <robin.laevaert@protonmail.com>
2023-12-13 22:49:41 +0100

    cleanup d13

--- Days/Days2023/Day_13/Day_13.cs
+++ Days/Days2023/Day_13/Day_13.cs
@@ -35,22 +35,7 @@ public class Day_13 : Day
 		var patternIndex = 0;
 		foreach (var pattern in Patterns)
 		{
-			var possibleLineSplits = Enumerable.Range(0, pattern.Lines.First().Length).ToList();
-			foreach (var line in pattern.Lines)
-			{
-				if (!possibleLineSplits.Any()) break;
-
-				possibleLineSplits = possibleLineSplits.Where((x,i) =>
-				{
-					var left = new string(line[..possibleLineSplits[i]].Reverse().ToArray());
-					var right = line[possibleLineSplits[i]..];
-
-					var isMirrorPossible = left.Any() && right.Any();
-					var smallestLength = Math.Min(left.Length, right.Length);
-					isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
-					return isMirrorPossible;
-				}).ToList();
-			}
+			var possibleLineSplits = pattern.Lines.Aggregate(Enumerable.Range(0, pattern.Lines.First().Length).ToList(), (current, line) => PossibleLineSplits(current, line));
 
 			if (possibleLineSplits.Count == 1)
 			{
@@ -59,60 +44,35 @@ public class Day_13 : Day
 			}
 			
 			var possibleColumnSplits = Enumerable.Range(0, pattern.Lines.Count).ToList();
-			for (int i = 0; i < pattern.Lines.First().Length; i++)
+			for (var i = 0; i < pattern.Lines.First().Length; i++)
 			{
 				if (!possibleColumnSplits.Any()) break;
 				
 				var str = pattern.Lines.Aggregate("", (current, line) => current + line[i]);
 				
-				possibleColumnSplits = possibleColumnSplits.Where((x,ind) =>
-				{
-					var left = new string(str[..possibleColumnSplits[ind]].Reverse().ToArray());
-					var right = str[possibleColumnSplits[ind]..];
-
-					var isMirrorPossible = left.Any() && right.Any();
-					var smallestLength = Math.Min(left.Length, right.Length);
-					isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
-					return isMirrorPossible;
-				}).ToList();
+				possibleColumnSplits = PossibleLineSplits(possibleColumnSplits, str);
 			}
 			if (possibleColumnSplits.Count == 1)
 			{
 				points += possibleColumnSplits.First() * 100;
-				continue;
 			}
 		}
 
 		return points.ToString();
 	}
-
+	
 	public override string HandlePart2()
 	{
 		var points = 0l;
-		var patternIndex = 0;
 		foreach (var pattern in Patterns)
 		{
 			var ogPatternPoints = 0;
-			var possibleLineSplits1 = Enumerable.Range(0, pattern.Lines.First().Length).ToList();
-			foreach (var line in pattern.Lines)
-			{
-				if (!possibleLineSplits1.Any()) break;
-				possibleLineSplits1 = possibleLineSplits1.Where((x,i) =>
-				{
-					var left = new string(line[..possibleLineSplits1[i]].Reverse().ToArray());
-					var right = line[possibleLineSplits1[i]..];
-
-					var isMirrorPossible = left.Any() && right.Any();
-					var smallestLength = Math.Min(left.Length, right.Length);
-					isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
-					return isMirrorPossible;
-				}).ToList();
-			}
+			var possibleLineSplits1 = pattern.Lines.Aggregate(
+				Enumerable.Range(0, pattern.Lines.First().Length).ToList(),
+				(current, line) => PossibleLineSplits(current, line));
 
 			if (possibleLineSplits1.Count == 1)
-			{
 				ogPatternPoints += possibleLineSplits1.First();
-			}
 
 			if (ogPatternPoints == 0)
 			{
@@ -123,16 +83,7 @@ public class Day_13 : Day
 				
 					var str = pattern.Lines.Aggregate("", (current, line) => current + line[i]);
 				
-					possibleColumnSplits1 = possibleColumnSplits1.Where((x,ind) =>
-					{
-						var left = new string(str[..possibleColumnSplits1[ind]].Reverse().ToArray());
-						var right = str[possibleColumnSplits1[ind]..];
-
-						var isMirrorPossible = left.Any() && right.Any();
-						var smallestLength = Math.Min(left.Length, right.Length);
-						isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
-						return isMirrorPossible;
-					}).ToList();
+					possibleColumnSplits1 = PossibleLineSplits(possibleColumnSplits1, str);
 				}
 				if (possibleColumnSplits1.Count == 1)
 				{
@@ -144,32 +95,12 @@ public class Day_13 : Day
 			var currPatternPoints = 0;
 			for (var p = 0; p < (pattern.Lines.Count * pattern.Lines.First().Length); p++)
 			{
-				var curPattern = pattern.Lines.ToList();
-				if (currPatternPoints != 0) continue;
-				var lineNumber = Convert.ToInt32(Math.Floor((decimal)p/curPattern.First().Length));
-				var charNumber = (p % curPattern.First().Length);
-				var charToSwitch = curPattern[lineNumber][charNumber];
-				curPattern[lineNumber] = curPattern[lineNumber].Remove(charNumber, 1)
-					.Insert(charNumber, charToSwitch == '#' ? "." : "#");
+				if (currPatternPoints != 0) break;
 				
-				var possibleLineSplits = Enumerable.Range(0, curPattern.First().Length).ToList();
-
-
-				foreach (var line in curPattern)
-				{
-					if (!possibleLineSplits.Any()) break;
-
-					possibleLineSplits = possibleLineSplits.Where((x,i) =>
-					{
-						var left = new string(line[..possibleLineSplits[i]].Reverse().ToArray());
-						var right = line[possibleLineSplits[i]..];
-
-						var isMirrorPossible = left.Any() && right.Any();
-						var smallestLength = Math.Min(left.Length, right.Length);
-						isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
-						return isMirrorPossible;
-					}).ToList();
-				}
+				var curPattern = SmudgePattern(pattern, p);
+				
+				var possibleLineSplits = curPattern.Aggregate(Enumerable.Range(0, curPattern.First().Length).ToList(),
+					(current, line) => PossibleLineSplits(current, line));
 				
 				if (possibleLineSplits.Count(x => x != ogPatternPoints) == 1)
 				{
@@ -178,28 +109,18 @@ public class Day_13 : Day
 				}
 			
 				var possibleColumnSplits = Enumerable.Range(0, curPattern.Count).ToList();
-				for (int i = 0; i < curPattern.First().Length; i++)
+				for (var i = 0; i < curPattern.First().Length; i++)
 				{
 					if (!possibleColumnSplits.Any()) break;
 				
 					var str = curPattern.Aggregate("", (current, line) => current + line[i]);
-				
-					possibleColumnSplits = possibleColumnSplits.Where((x,ind) =>
-					{
-						var left = new string(str[..possibleColumnSplits[ind]].Reverse().ToArray());
-						var right = str[possibleColumnSplits[ind]..];
 
-						var isMirrorPossible = left.Any() && right.Any();
-						var smallestLength = Math.Min(left.Length, right.Length);
-						isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
-						return isMirrorPossible;
-					}).ToList();
+					possibleColumnSplits = PossibleLineSplits(possibleColumnSplits, str);
 				}
 
 				if (possibleColumnSplits.Count(x => x*100 != ogPatternPoints) == 1)
 				{
 					currPatternPoints += possibleColumnSplits.First(x => x*100 != ogPatternPoints) * 100;
-					continue;
 				}
 			}
 
@@ -207,10 +128,35 @@ public class Day_13 : Day
 			{
 				points += currPatternPoints;
 			}
-			patternIndex++;
 		}
 
 		return points.ToString();
+	}
+	
+	private static List<int> PossibleLineSplits(List<int> possibleLineSplits, string line)
+	{
+		return possibleLineSplits.Where((x,i) =>
+		{
+			var left = new string(line[..possibleLineSplits[i]].Reverse().ToArray());
+			var right = line[possibleLineSplits[i]..];
+
+			var isMirrorPossible = left.Any() && right.Any();
+			var smallestLength = Math.Min(left.Length, right.Length);
+			isMirrorPossible &= left[..smallestLength].SequenceEqual(right[..smallestLength]);
+			return isMirrorPossible;
+		}).ToList();
+	}
+
+	private static List<string> SmudgePattern(Pattern pattern, int p)
+	{
+		var curPattern = pattern.Lines.ToList();
+
+		var lineNumber = Convert.ToInt32(Math.Floor((decimal)p/curPattern.First().Length));
+		var charNumber = (p % curPattern.First().Length);
+		var charToSwitch = curPattern[lineNumber][charNumber];
+		curPattern[lineNumber] = curPattern[lineNumber].Remove(charNumber, 1)
+			.Insert(charNumber, charToSwitch == '#' ? "." : "#");
+		return curPattern;
 	}
 }
 
